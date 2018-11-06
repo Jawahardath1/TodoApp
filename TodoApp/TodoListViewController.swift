11b099866aa0ebf97e48c2b0d5b70b877ebfb5bb
Dath <jawahar@jawahars-macbook-pro.local>
2018-11-06 13:44:16 -0500

    Adding Datasource and Delegate Methods

---
+++ TodoApp/TodoListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TodoListViewController.swift
+//  TodoApp
+//
+//  Created by Jawahar T on 11/6/18.
+//  Copyright © 2018 Jawahar T. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Item 1", "Item 2", "Item 3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
